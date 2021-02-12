b0b02db763bbcbaacf33a0187c550a98eceb3dca
Gregorio <gregorio.jimenez@netberry.es>
2021-02-12 17:28:23 +0100

    [FIX] Correccion al realizar una accion en lote, objetos extra añadidos

--- src/app/examples/components/batch-actions/batch-actions.component.ts
+++ src/app/examples/components/batch-actions/batch-actions.component.ts
@@ -3,34 +3,33 @@ import { NbTableSorterHeader } from '../../../../../public_api';
 import { MockedUsersService } from '../../../mocked-users.service';
 
 @Component({
-	selector: 'nb-batch-actions',
-	templateUrl: './batch-actions.component.html',
-	styleUrls: ['./batch-actions.component.scss']
+    selector: 'nb-batch-actions',
+    templateUrl: './batch-actions.component.html',
+    styleUrls: ['./batch-actions.component.scss']
 })
 export class BatchActionsComponent implements OnInit {
 
-	items: any[];
-	headers: (NbTableSorterHeader | string)[] = [
-		'id',
-		'username',
-		'email',
-		'name'
-	];
-	searchKeys: string[] = ['id', 'username', 'email', 'name'];
+    items: any[];
+    headers: (NbTableSorterHeader | string)[] = [
+        'id',
+        'username',
+        'email',
+        'name'
+    ];
+    searchKeys: string[] = ['id', 'username', 'email', 'name'];
 
-	batchActions = [
-		{
-			title: 'export',
-			handler: (items) => console.log('export', items)
-		},
-	]
+    batchActions = [
+        {
+            title: 'export',
+            handler: (items) => console.log('export', items)
+        }
+    ];
 
-	constructor(
-		private _mockedUsersSvc: MockedUsersService
-	) { }
+    constructor(
+        private _mockedUsersSvc: MockedUsersService
+    ) { }
 
-	ngOnInit() {
-		this.items = this._mockedUsersSvc.items;
-	}
-
+    ngOnInit() {
+        this.items = this._mockedUsersSvc.items;
+    }
 }
