4275d5ab8a719fcb6c42bd3b984bdf092c6b7eb5
Carlos Morcillo <carlos.morcillo@me.com>
2025-05-21 09:53:28 +0200

    feat(paginable-table-dropdown): refactor dropdown component to use toggle method and improve rendering logic

--- src/lib/components/paginable-table-dropdown/paginable-table-dropdown.component.ts
+++ src/lib/components/paginable-table-dropdown/paginable-table-dropdown.component.ts
@@ -2,8 +2,12 @@ import { NgClass, NgStyle } from '@angular/common';
 import {
 	Component,
 	ElementRef,
+	EmbeddedViewRef,
 	HostListener,
 	Input,
+	TemplateRef,
+	ViewChild,
+	ViewContainerRef,
 	inject
 } from '@angular/core';
 import { PaginableTableDropdown } from '../../interfaces/paginable-table-dropdown';
@@ -11,6 +15,7 @@ import { HubIconComponent } from '../icon/icon.component';
 
 @Component({
 	selector: 'hub-table-dropdown, paginable-table-dropdown',
+
 	standalone: true,
 	imports: [NgClass, NgStyle, HubIconComponent],
 	templateUrl: './paginable-table-dropdown.component.html',
@@ -18,6 +23,12 @@ import { HubIconComponent } from '../icon/icon.component';
 })
 export class PaginableTableDropdownComponent<T = any> {
 	private _elementRef = inject(ElementRef);
+
+	@ViewChild('dropdownTpt') dropdownTpt!: TemplateRef<any>;
+
+	private vcr = inject(ViewContainerRef);
+	private embeddedView: EmbeddedViewRef<any> | null = null;
+	private renderedElement: HTMLElement | null = null;
 
 	@Input() item: T | undefined;
 
@@ -44,6 +55,8 @@ export class PaginableTableDropdownComponent<T = any> {
 		}
 	}
 
+	@Input() appendTo: HTMLElement | 'body' | null = 'body';
+
 	@Input() disabled: boolean = false;
 
 	buttonClass: string | null = null;
@@ -66,10 +79,57 @@ export class PaginableTableDropdownComponent<T = any> {
 		}
 	}
 
+	toggle() {
+		if (this.shown) {
+			this.close();
+			return;
+		}
+
+		this.shown = true;
+
+		const target =
+			this.appendTo === 'body'
+				? document.body
+				: this.appendTo instanceof HTMLElement
+					? this.appendTo
+					: this._elementRef.nativeElement;
+
+		// Crea la vista
+		this.embeddedView = this.vcr.createEmbeddedView(this.dropdownTpt);
+		this.embeddedView.detectChanges();
+
+		const [element] = this.embeddedView.rootNodes as HTMLElement[];
+		this.renderedElement = element;
+
+		const button = this._elementRef.nativeElement.querySelector('button');
+		const rect = button.getBoundingClientRect();
+
+		Object.assign(element.style, {
+			position: 'absolute',
+			zIndex: '1050',
+			top: `${rect.bottom + window.scrollY}px`,
+			left: `${rect.left + window.scrollX}px`
+		});
+
+		target.appendChild(element);
+	}
+
 	/**
 	 * Sets the "shown" property to false.
 	 */
 	close() {
 		this.shown = false;
+
+		if (this.embeddedView) {
+			this.embeddedView.destroy();
+			this.embeddedView = null;
+		}
+
+		if (this.renderedElement && this.renderedElement.parentElement) {
+			this.renderedElement.parentElement.removeChild(
+				this.renderedElement
+			);
+			this.renderedElement = null;
+		}
 	}
 }
